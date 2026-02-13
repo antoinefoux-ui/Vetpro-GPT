4a63dcefe1c51d9c569a1948fc03e98ebafd2885
antoinefoux-ui <antoine.foux@gmail.com>
2026-02-13 10:53:15 +0100

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    strictPort: false,
+    hmr: {
+      clientPort: 443
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false
   }
 })
